10b0876ca0e8bb24886e9b6e8cd5f5cf1d3a2fb6
Joyt <bigboom1122@gmail.com>
2023-05-16 11:51:05 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,14 @@
 import { Command } from 'commander';
-import { makeStatus } from '../src/index.js';
+import { notAsame } from '../src/index.js';
 const program = new Command();
 
 program
     .name('gendiff')
     .description('Compares two files and shows a difference.')
-    .version('0.0.1')
     .option('-f, --format <type>', 'output format')
     .arguments('<filepath1> <filepath2>')
-    .action(makeStatus)
+    .action(notAsame);
+    
     
 program.parse();
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//import _ from "lodash";
+import _ from "lodash";
 import { readFileSync } from 'node:fs';
 
 
@@ -10,7 +10,6 @@ export function isEqual(object1, object2) {
   if (props1.length !== props2.length) {
     return false;
   }
-
   for (let i = 0; i < props1.length; i += 1) {
     const prop = props1[i];
     const bothAreObjects = typeof(object1[prop]) === 'object' && typeof(object2[prop]) === 'object';
@@ -20,7 +19,7 @@ export function isEqual(object1, object2) {
       return false;
     }
   }
-
+  
   return true;
 };
 //создаёт массив со статусами объектов
@@ -29,29 +28,32 @@ export const makeStatus = (file1, file2) => {
   const keysfile2 = Object.keys(_.cloneDeep(file2));
   const keys = _.uniqWith(keysfile1.concat(keysfile2), _.isEqual).sort();
   const finish =[];
-  const keysStatus = keys.map((key) => {
+ keys.forEach((key) => {
     if (file1[key] === file2[key]) finish.push({key: key, value: file1[key], status: 'same'});
     if(_.has(file1, key) && _.has(file2, key) && file2[key] !== file1[key] ) finish.push({key: key, value1: file1[key], value2: file2[key], status: 'samekey'});
-    if(_.has(file1, key) && !_.has(file2, key)) finish.push({key: key, value: file1[key], status: 'File1_Only'});
-    if(_.has(file2, key) && !_.has(file1, key)) finish.push({key: key, value: file2[key], status: 'File2_Only'});
+    if(_.has(file1, key) && !_.has(file2, key)) finish.push({key: key, value: file1[key], status: 'File1'});
+    if(_.has(file2, key) && !_.has(file1, key)) finish.push({key: key, value: file2[key], status: 'File2'});
   });
+  console.log(finish);
   return finish;
 };
 
 //финальный конструктор
-export const notAsame = (fileN1, fileN2) => {
-  if (isEqual(fileN1, fileN2)) return JSON.stringify(fileN1);
-  const result = {}; 
-  const status = makeStatus(fileN1, fileN2).map((obj) => {
-    if (obj.status === 'File1_Only') result['- ' + obj.key] = obj.value;
-    if (obj.status === 'same') result[obj.key] = obj.value;
-    if (obj.status === 'File2_Only') result['+ ' + obj.key] = obj.value;
+export const notAsame = (path1, path2) => {
+  const toFile1 = readFileSync(path1, 'utf-8');
+  const toFile2 = readFileSync(path2, 'utf-8');
+  const objFile1 = JSON.parse(toFile1);
+  const objFile2 = JSON.parse(toFile2);
+  if (isEqual(objFile1, objFile2)) return JSON.stringify(objFile1);
+  const status = makeStatus(objFile1, objFile2).map((obj) => {
+    if (obj.status === 'File1') return `${['- ' + obj.key]}: ${obj.value}`;
+    if (obj.status === 'same') return `${['  ' + obj.key]}: ${obj.value}`;
+    if (obj.status === 'File2') return `${['+ ' + obj.key]}: ${obj.value}`;
     if (obj.status === 'samekey') {
-      result['- ' + obj.key] = obj.value1;
-      result['+ ' + obj.key] = obj.value2;
+      const newString = `${['- ' + obj.key]}: ${obj.value1}\n${['+ ' + obj.key]}: ${obj.value2}`; 
+      return newString;
     }
   });
-  return JSON.stringify(result);
+  console.log(`{\n${status.join('\n')}\n}`);
 }
 
-console.log(notAsame(filee1, filee2));
