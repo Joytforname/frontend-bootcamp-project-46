0930886fa6f408e67f29daba93a2a6a6cba28e84
Joyt <bigboom1122@gmail.com>
2023-05-18 15:11:44 +0300

    step 5

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,25 @@
 import { test, expect } from '@jest/globals';
 import { notAsame, isEqual } from '../src/index.js';
 import { sample1, sample2 } from '../__fixtures__/fortest.js';
+import { formatParse } from '../src/parsers.js';
 
 test('notAsame', () => {
   expect(notAsame('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(sample1);
+});
+
+test('formatParse', () => {
+  expect(formatParse('./__fixtures__/file1.json')).toEqual({
+    follow: false,
+    host: 'hexlet.io',
+    proxy: '123.234.53.22',
+    timeout: 50,
+  });
+  expect(formatParse('./__fixtures__/file1.yml')).toEqual({
+    host: 'hexlet.io,',
+    timeout: '50,',
+    proxy: '123.234.53.22,',
+    follow: false
+  });
 });
 
 test('isEqual', () => {
--- src/parsers.js
+++ src/parsers.js
@@ -8,3 +8,4 @@ export const formatParse = (fileForParse) => {
   if (format(fileForParse) === '.json') return JSON.parse(readFileSync(fileForParse), 'utf-8');
   if (format(fileForParse) === '.yml' || format(fileForParse) === '.yaml') return yaml.load(readFileSync(fileForParse), 'utf8');
 };
+
